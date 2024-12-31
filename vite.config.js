09c782a7164887c131bd71affa1ae4ee78ee3032
alighafari <alighafari.code@gmail.com>
2024-12-31 22:56:49 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-unused-vars */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   base: '/Contact-Manager/',
 });
