5d84bed5e8b3cb07802bc544f19270f11c1e52b2
alighafari <alighafari.code@gmail.com>
2025-01-01 15:48:19 +0330

    correct code

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
           <Routes>
             <Route path="/" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
-
             <Route
               path="/users"
               element={
